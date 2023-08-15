42b88ae9c9221f200be5e121b502276d669c6437
TheusMarqs <matheusmarqsilva@gmail.com>
2023-08-15 16:46:22 -0300

    insert e buscas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_PRODUCT (PRICE, NAME) VALUES (200, 'Boneca sensual inflável de borracha');
+INSERT INTO TBL_PRODUCT (PRICE, NAME) VALUES (15000, 'Rim humano pela metade');
+INSERT INTO TBL_PRODUCT (PRICE, NAME) VALUES (20000, 'Coração humano funcionando meia boca');
